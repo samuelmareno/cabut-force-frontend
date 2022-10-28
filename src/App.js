21a852bcd2e05666ff960ee469d6316d8a20d69b
Samuel Mareno <samuelmareno777@gmail.com>
2022-10-28 21:51:35 +0700

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./screens/dashboard/components/Navbar";
+import Sidebar from "./screens/dashboard/components/Sidebar";
 import DashboardScreen from "./screens/dashboard/DashboardScreen";
-import LoginScreen from './screens/login/LoginScreen'
+import LoginScreen from "./screens/login/LoginScreen";
 
 function App() {
-    return (
-    <DashboardScreen/>
-);
+  return (
+    <BrowserRouter>
+    <Navbar />
+    <Sidebar />
+      </BrowserRouter>
+  );
 }
 
 export default App;
--- src/screens/dashboard/components/Sidebar.jsx
+++ src/screens/dashboard/components/Sidebar.jsx
@@ -1,20 +1,90 @@
 import React from "react";
-import {MenuItems, items } from "./MenuItems";
-//import { Link, NavLink } from "react-router-dom";
+import { MenuItems, items } from "./MenuItems";
+import { Link, NavLink } from "react-router-dom";
 
-//import { useStateContext } from "../contexts/ContextProvider";
+import { useStateContext } from "../../../contexts/ContextProvider";
 
-export default function Sidebar({selectedMenu, expanded, handleClick}) {
-    return (
-        <div
-            className={`${expanded ? 'w-72' : 'hidden'} flex static flex-col justify-between font-bold text-xl bg-primary-600 h-screen`}>
-            <ul>
-                {MenuItems(selectedMenu, handleClick)}
-            </ul>
-            <div className="p-4 flex items-center justify-evenly w-full text-white bg-blue-500 hover:bg-blue-400 cursor-pointer duration-300">
-                <a href='#'>Logout</a>
-                <a href='#' className="material-symbols-rounded font-bold">logout</a>
+const Sidebar = () => {
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
+
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg font-semibold bg-blue-700 text-white text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-black hover:text-black hover:bg-gray-100 m-2 duration-300";
+
+  return (
+    <div
+      className="ml-3 h-screen
+      md:overflow-hidden overflow-auto 
+      md:hover:overflow-auto pb-10"
+    >
+      {activeMenu && (
+        <>
+          <div id="Logo Brand" className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={handleCloseSidebar}
+              className="items-center gap-3 ml-3 
+              mt-4 flex text-xl font-extrabold
+              text-slate-900"
+            >
+              <div className="flex items-center text-xl font-semibold text-gray-900">
+                <img
+                  alt="logo"
+                  className="w-8 h-8 mr-2"
+                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+                />
+                Cabut Force
+              </div>
+            </Link>
+            <button
+              id="Close Sidebar Button"
+              type="button"
+              onClick={() => setActiveMenu(!activeMenu)}
+              className="text-xl rounded-full text-black
+              p-3 hover:bg-light-gray mt-4 block md:hidden
+              material-symbols-rounded"
+            >
+              cancel
+            </button>
+          </div>
+          <div id="Dashboard Menu" className="mt-10">
+            {items.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={handleCloseSidebar}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
+          </div>
+          <div className="">
+            <div
+              className={`flex-grid font-bold flex items-center gap-3 py-4 m-3 rounded-lg text-md text-gray-700`}
+            >
+              <button className="capitalize">Logout</button>
+              <span className="material-symbols-rounded">logout</span>
             </div>
-        </div>
-    );
-}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
